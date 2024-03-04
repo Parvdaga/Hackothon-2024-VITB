757a1668380ec332de3e4268d81417ea4533af60
Suyyash_Ar <suyyasha@gmail.com>
2024-03-04 10:00:39 +0530

    Backend routing

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,19 @@
-import express from "express";
-import mongoose from "mongoose";
-import { PORT, mongoDBURL } from "./config.js";
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const mongoDBURL = "Link";
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile("");
+  res.sendFile(path.join(__dirname, "..", "Frontend", "index.html"));
+});
+app.get("/index(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "Frontend", "index.html"));
+});
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "Frontend", "404.html"));
 });
 
 app.listen(PORT, () => {
